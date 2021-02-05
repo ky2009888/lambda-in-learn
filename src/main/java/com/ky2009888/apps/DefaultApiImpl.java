c378beca8f1585fe22cfc10b5e54f7702a945c35
cf_jijinliang <ji_jinliang2012@163.com>
2021-02-05 16:24:05 +0800

    新增标准格式的默认接口方法代码

--- src/main/java/com/ky2009888/apps/DefaultApiImpl.java
+++ src/main/java/com/ky2009888/apps/DefaultApiImpl.java
@@ -3,6 +3,9 @@ package com.ky2009888.apps;
 import com.ky2009888.apps.api.DefaultApiV1;
 import com.ky2009888.apps.api.DefaultApiV2;
 
+/**
+ * @author Lenovo
+ */
 public class DefaultApiImpl implements DefaultApiV1, DefaultApiV2 {
     @Override
     public String toUpcase(String targetStr) {
--- src/main/java/com/ky2009888/apps/api/DefaultApiV2.java
+++ src/main/java/com/ky2009888/apps/api/DefaultApiV2.java
@@ -1,5 +1,8 @@
 package com.ky2009888.apps.api;
 
+/**
+ * @author Lenovo
+ */
 public interface DefaultApiV2 extends DefaultApi{
     public String showBstr();
 }
